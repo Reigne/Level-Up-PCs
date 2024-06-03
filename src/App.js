84fcc9f19a771705677121272440970b6d985b6a
Reigne <monteronaelija@yahoo.com>
2024-06-03 21:54:36 +0800

    add authentication

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
+import React, { useEffect } from "react";
+import { useSelector } from "react-redux";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home/Home";
 import Navbar from "./components/Navbar/Navbar";
+import ProtectedRoute from "./components/route/ProtectedRoute";
 import Login from "./pages/Login/Login";
 import SignUp from "./pages/SignUp/SignUp";
 import Products from "./pages/Product/Products";
@@ -8,11 +11,19 @@ import ProductForm from "./pages/Product/ProductForm";
 import Categories from "./pages/Category/Categories";
 import Brands from "./pages/Brand/Brands";
 
-import { PrimeReactProvider, PrimeReactContext } from 'primereact/api';
+import { PrimeReactProvider, PrimeReactContext } from "primereact/api";
 import "primereact/resources/themes/lara-light-cyan/theme.css";
 
+import { loadUser } from "./actions/userActions";
+import store from "./store";
 
 export default function App() {
+  useEffect(() => {
+    store.dispatch(loadUser());
+  }, []);
+
+  const { user, isAuthenticated, loading } = useSelector((state) => state.auth);
+
   return (
     <div className="flex flex-col h-screen">
       <Navbar />
@@ -25,6 +36,15 @@ export default function App() {
         <Route path="/product/create" element={<ProductForm />} />
         <Route path="/categories" element={<Categories />} />
         <Route path="/brands" element={<Brands />} />
+
+        <Route
+          path="/profile"
+          element={
+            <ProtectedRoute>
+              <Products />
+            </ProtectedRoute>
+          }
+        />
       </Routes>
     </div>
   );
